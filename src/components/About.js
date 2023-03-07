47e3bf55d193765d31af8782816196cb82a4c441
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-06 22:14:19 -0700

    about added, apparel array formatting added to json

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,33 @@ function About() {
   return (
     <div>
         <h1>About Us</h1>
+        <h2>
+          Hello valued customer ᨒ </h2>
+          <p>
+          Ever since (store name) was founded in March of 2023, we have been providing the best gear for the steeziest shredders.
+          Whether you need something skinny to rip groomers or a wide setup for those big powder days we have everything to meet your needs.
+           Alongside our extensive ski and snowboard offerings, we also offer premier apparel including socks, baselayers, midlayers, shells
+            and even insulated jackets. At (store name), we have everything to get you out on your favorite mountain and making great memories with your
+            friends and families.
+        </p>
+        <br>
+        </br>
+        <h2>
+        We are here to answer any of your questions!
+          </h2>
+        <br></br>
+        <h2>
+        Contact us:
+        </h2>
+        <br>
+        </br>
+        Phone - (555)-(555)-(555)
+        <br>
+        </br>
+        Email - (storename)@flatironschool.com 
+        <br>
+        </br>
+        Address: 225 Mountain Drive, Telluride, Colorado, 81435
     </div>
   )
 }
