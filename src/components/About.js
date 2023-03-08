dbd10926a97de56139496c91bb61762bfc17400a
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-07 19:51:03 -0700

    filled in home section

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ function About() {
         <h2>
           Hello valued customer ᨒ </h2>
           <p>
-          Ever since (store name) was founded in March of 2023, we have been providing the best gear for the steeziest shredders.
+          Ever since Icebox Ski Shop was founded in March of 2023, we have been providing the best gear for the steeziest shredders.
           Whether you need something skinny to rip groomers or a wide setup for those big powder days we have everything to meet your needs.
            Alongside our extensive ski and snowboard offerings, we also offer premier apparel including socks, baselayers, midlayers, shells
-            and even insulated jackets. At (store name), we have everything to get you out on your favorite mountain and making great memories with your
+            and even insulated jackets. At Icebox Ski Shop, we have everything to get you out on your favorite mountain and making great memories with your
             friends and families.
         </p>
         <br>
@@ -27,7 +27,7 @@ function About() {
         Phone - (555)-(555)-(555)
         <br>
         </br>
-        Email - (storename)@flatironschool.com 
+        Email - support@iceboxskishop.com 
         <br>
         </br>
         Address: 225 Mountain Drive, Telluride, Colorado, 81435
