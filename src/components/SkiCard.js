d394564a849c965415270c001606221ad2c5a8c5
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-09 00:06:46 -0700

    rough styling to all cards is done

--- src/components/SkiCard.js
+++ src/components/SkiCard.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { Button, Card } from 'react-bootstrap';
 
 
 function SkiCard({removeSkis, countLikes, likes, brand, price, gender, description, image, year, id}) {
@@ -28,16 +29,25 @@ function SkiCard({removeSkis, countLikes, likes, brand, price, gender, descripti
   }
    
   return (
-    <div id={id} >
-        <h2>{brand} <br/> {gender}</h2>
-        <img src={image} alt={description}/>
-        <p>{description} <br/>{year}<br/> </p>
-        <p>{`$ ${price}.00`}</p>
-        <button onClick={addLikes}>💙 <br/> {skiLikes}</button>
-        <button>Add to Cart</button><small><button onClick={()=>handleDelete(id)}>X</button></small>
-
-
-    </div>
+    <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Title>{brand}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{gender}</Card.Subtitle>
+        <Card.Img src={image} alt={description} />
+        <Card.Text>
+          {description} <br />
+          {year}
+        </Card.Text>
+        <Card.Text>{`$ ${price}.00`}</Card.Text>
+        <Button variant="primary" onClick={addLikes}>
+          💙 {skiLikes}
+        </Button>
+        <Button variant="success">Add to Cart</Button>
+        <Button variant="danger" onClick={() => handleDelete(id)}>
+          X
+        </Button>
+      </Card.Body>
+    </Card>
   )
 }
 
--- src/components/SnowboardCard.js
+++ src/components/SnowboardCard.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {Button, Card} from 'react-bootstrap'
 
 function SnowboardCard({countLikes, likes, removeBoard, brand, price, gender, description, image, year, id}) {
   const [boardLikes, setBoardLikes] = useState(likes)
@@ -26,15 +27,25 @@ function SnowboardCard({countLikes, likes, removeBoard, brand, price, gender, de
   }
 
   return (
-    <div id={id}>
-        <h2>{brand} | {gender}</h2>
-        <img src={image} alt={description}/>
-        <p>{description} <br/>{year}<br/> </p>
-        <p>{`$ ${price}.00`} </p>
-        <button onClick={addLikes}>💙 <br/> {boardLikes}</button>
-        <button>Add to Cart</button><small><button onClick={()=>handleDelete(id)}>X</button></small>
-
-    </div>
+<Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Title>{brand}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{gender}</Card.Subtitle>
+        <Card.Img src={image} alt={description} />
+        <Card.Text>
+          {description} <br />
+          {year}
+        </Card.Text>
+        <Card.Text>{`$ ${price}.00`}</Card.Text>
+        <Button variant="primary" onClick={addLikes}>
+          💙 {boardLikes}
+        </Button>
+        <Button variant="success">Add to Cart</Button>
+        <Button variant="danger" onClick={() => handleDelete(id)}>
+          X
+        </Button>
+      </Card.Body>
+    </Card>
   )
 }
 
