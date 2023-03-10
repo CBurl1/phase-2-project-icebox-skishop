62e702f1097a782ddb2d81da64707bb6efdea4a7
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-10 00:43:45 -0700

    styled storefront - last of changes manually added after branch issue

--- src/components/Storefront.js
+++ src/components/Storefront.js
@@ -1,20 +1,25 @@
 import Snowboard from "./Snowboard"
 import Ski from "./Ski"
 import Apparel from "./Apparel"
-import React from 'react'
+import { Row, Col } from 'react-bootstrap';
 
 function Storefront() {
-
   return (
-
-
     <div>
-      <Snowboard   />
-      <Ski  />
-      <Apparel />
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={4}>
+          <Snowboard />
+        </Col>
+        <Col xs={12} md={4}>
+          <Ski />
+        </Col>
+        <Col xs={12} md={4}>
+          <Apparel />
+        </Col>
+      </Row>
     </div>
-
-
   )
 }
-export default Storefront
+
+export default Storefront;
+
