24f0e0c10ac4c41bb1db4e24ae90182bc7c6a832
Connor Sheets <connorsheets32@gmail.com>
2023-03-10 06:05:32 -0600

    good morning Shredder

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,13 +9,13 @@ function Home() {
           <h1>Home Page</h1>
           <h2>Welcome to Icebox Ski Shop!</h2>
           <p>
-            What's up shredder? Let's be real, if you're here shopping with us then you are the real deal! If want to ski the toughest lines with the best and safest gear then you don't have to look any further.
+            What's up Shredder? Let's be real, if you're here shopping with us then you are the real deal! If you want to ski the toughest lines with the best and safest gear then you don't have to look any further.
             <br />
             <br />
             Check out our "About" section to learn more about Icebox
             <br />
             <br />
-            Check out our Storefront to see all the latest and greatest gear. If you are feeling a little lost, you can always get back to the Storefront!
+            Check out our Shop to see all the latest and greatest gear. If you are feeling a little lost, just head there!
             <br />
             <br />
             Don't be shy, shop around! We have everything to meet your needs here at the newest ski shop around!
