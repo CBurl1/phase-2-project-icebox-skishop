b00a1ca7440648a81ea7810c920b2b9f5c2c25c1
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-10 00:33:54 -0700

    styled about

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function About() {
   return (
-    <div>
-        <h1>About Us</h1>
-        <h2> Hello valued customer ᨒ </h2>
+    <Container>
+      <Row>
+        <Col>
+          <h1>About Us</h1>
+          <h2> Hello valued customer ᨒ </h2>
           <p>
-          Ever since Icebox Ski Shop was founded in March of 2023, we have been providing the best gear for the steeziest shredders.
-          Whether you need something skinny to rip groomers or a wide setup for those big powder days we have everything to meet your needs.
-           Alongside our extensive ski and snowboard offerings, we also offer premier apparel including socks, baselayers, midlayers, shells
-            and even insulated jackets. At Icebox Ski Shop, we have everything to get you out on your favorite mountain and making great memories with your
-            friends and families.
-        </p>
-        <br />
-        <h2>We are here to answer any of your questions!</h2>
-        <br />
-        <h2> Contact us:</h2>
-        <br />
-        Phone - (555)-(555)-(5555)
-        <br />
-        Email - support@iceboxskishop.com
-        <br />
-        Address: 225 Mountain Drive, Telluride, Colorado, 81435
-    </div>
-  )
+            Ever since Icebox Ski Shop was founded in March of 2023, we have been providing the best gear for the steeziest shredders.
+            Whether you need something skinny to rip groomers or a wide setup for those big powder days we have everything to meet your needs.
+            Alongside our extensive ski and snowboard offerings, we also offer premier apparel including socks, baselayers, midlayers, shells
+            and even insulated jackets. At Icebox Ski Shop, we have everything to get you out on your favorite mountain and making great memories with your friends and families.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>We are here to answer any of your questions!</h2>
+          <br />
+          <h2> Contact us:</h2>
+          <br />
+          <p>
+            Phone - (555)-(555)-(5555)
+            <br />
+            Email - support@iceboxskishop.com
+            <br />
+            Address: 225 Mountain Drive, Telluride, Colorado, 81435
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default About
+export default About;
